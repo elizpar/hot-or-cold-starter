e56528b4ef98060537c37647bcd6f90a3f8ce246
Liz <elizpar@gmail.com>
2015-06-03 18:17:52 -0400

    intial commit

--- js/app.js
+++ js/app.js
@@ -4,13 +4,31 @@ $(document).ready(function(){
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
-
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+$('.new').on('click', function(event){
+  location.reload();
+});
+
+var randomNum= Math.floor((Math.random() * 100) + 1);
+
+$('#guessButton').on('click', function(event) {
+    event.preventDefault();
+    var userEntry = $('#userGuess').val();
+      $('#guessList').append('<li><span>' +userEntry+ '</span></li>');
+      $('#userGuess').val("");
+      $('#count').html('1');     
+  });
+
+if (randomNum == userEntry) {
+  $('#feedback').html('Sweet');
+}
+if (randomNum > userEntry + 75 && randomNum )
+
 
 });
 
