4ce533e423a991f0d73f3388eb8e135805dfb560
Liz <elizpar@gmail.com>
2015-06-12 16:26:13 -0400

    Nan

--- js/app.js
+++ js/app.js
@@ -1,34 +1,63 @@
-
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  
+  /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
+    });
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
 
 $('.new').on('click', function(event){
   location.reload();
+});
 
-//generate random number on page load
+var counter= 0;
 var randomNum= Math.floor((Math.random() * 100) + 1);
+console.log(randomNum);
 
 $('#guessButton').on('click', function(event) {
     event.preventDefault();
+
     var userEntry = $('#userGuess').val();
+    if (userEntry % 1 == 0 && userEntry < 100 && userEntry > 1){ 
       $('#guessList').append('<li><span>' +userEntry+ '</span></li>');
       $('#userGuess').val("");
-      $('#count').html('1');     
+      counter++;
+      $('#count').html(counter);
+
+      }
+
+    else {
+
+    alert('Enter a number between 1 and 100!');
+         }   
   });
 
-if (randomNum == userEntry) {
-  $('#feedback').html('Sweet');
-}
-if (randomNum > userEntry + 75 && randomNum )
+/* if (userEntry == randomNum) {
+  $('#feedback').html('Sweet.');
+  }
+  else if (userEntry - randomNum < randomNum + 10) {
+  $('#feedback').html('Very hot!');
+  }
+else if (userEntry - randomNum < 10) && randomNum + 10) {
+  $('#feedback').html('Mmm, close.');
+  }
+else if (userEntry - randomNum < randomNum + 10) {
+  $('#feedback').html('Cool.');
+  }
+else if (userEntry - randomNum < randomNum + 10) {
+  $('#feedback').html('Brrr');
+  }
+else if (userEntry - randomNum < randomNum + 10) {
+  $('#feedback').html('Ice cold!');
+  }
+
+  ); check for hot first... order I want to makes these test... what makes a
+what are the necessary tests to run? check match first
+check for something NaN or outside ... do it with javascript
+
+*/
 
 });
-});
-
